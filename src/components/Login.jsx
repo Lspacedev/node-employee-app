41060dbb1ba090dcae979a85d67ee5a10156354d
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2024-10-04 12:25:03 +0200

    added login and register page

---
+++ src/components/Login.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../config/firebase";
+
+function AdminLogin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigation = useNavigate();
+
+  function login() {
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        alert("Log in successfully");
+        navigation("/home");
+      })
+      .catch((err) => {});
+  }
+
+  return (
+    <div className="Login">
+      <div className="login-img">
+        {/* <img src="images/login-register.jpg" alt="login" /> */}
+      </div>
+      <div className="login-form-container">
+        <h2>Employee Manager | Admin</h2>
+        <p>Log in to your admin account.</p>
+        <div className="form">
+          <div className="email">
+            <label htmlFor="email">
+              Email:
+              <input
+                type="email"
+                id="email"
+                name="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+          </div>
+
+          <div className="password">
+            <label htmlFor="password">
+              Password:
+              <input
+                type="password"
+                id="password"
+                name="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+          </div>
+
+          <button className="submit-btn" onClick={login}>
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AdminLogin;
