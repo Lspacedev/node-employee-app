35e9b5ad24fc0fd0a0b644e62af353bbf944bad7
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2024-10-17 15:35:17 +0200

    added comment

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -17,6 +17,23 @@ function AdminLogin() {
       credentials: "include",
     });
   }
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == " ") {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  console.log(getCookie("_csrf"));
+  console.log(getCookie("XSRF-TOKEN"));
   console.log(Cookies.get("_csrf"));
   console.log(Cookies.get("XSRF-TOKEN"));
   function login() {
